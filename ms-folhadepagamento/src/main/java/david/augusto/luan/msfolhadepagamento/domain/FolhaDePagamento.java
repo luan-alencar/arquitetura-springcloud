7564d2caff852e113a46554e1d7bca8a7e124b76
luan-alencar <luanalencar134@gmail.com>
2021-12-28 14:36:10 -0300

    retirando dependencias de BD em ms-folhadepagamento

--- ms-folhadepagamento/src/main/java/david/augusto/luan/msfolhadepagamento/domain/FolhaDePagamento.java
+++ ms-folhadepagamento/src/main/java/david/augusto/luan/msfolhadepagamento/domain/FolhaDePagamento.java
@@ -1,32 +1,28 @@
 package david.augusto.luan.msfolhadepagamento.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
 import java.io.Serializable;
 
-@Entity
-@Table(name = "TB_FOLHA_PAGAMENTO")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class FolhaDePagamento implements Serializable {
     private static final long serialVersionUID = 1l;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
-
-    @Column(name = "nome")
     private String nome;
-
-    @Column(name = "salario_dia")
     private Double salarioPorDia;
-
-    @Column(name = "dias")
     private Integer dias;
 
     public double getTotal() {
         return this.getDias() * this.getSalarioPorDia();
     }
 }
+
+
+
--- ms-folhadepagamento/src/main/java/david/augusto/luan/msfolhadepagamento/service/dto/FolhaDePagamentoDTO.java
+++ ms-folhadepagamento/src/main/java/david/augusto/luan/msfolhadepagamento/service/dto/FolhaDePagamentoDTO.java
@@ -1,12 +1,16 @@
 package david.augusto.luan.msfolhadepagamento.service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class FolhaDePagamentoDTO implements Serializable {
 
     private Long id;
