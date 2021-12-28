4b36b9a359e942f9dbd82d679282bb72106580c6
luan-alencar <luanalencar134@gmail.com>
2021-12-28 13:50:51 -0300

    esqueleto do ms-folhadepagamento gerado - aplicacao rodando

---
+++ ms-folhadepagamento/src/main/java/david/augusto/luan/msfolhadepagamento/MsFolhadepagamentoApplication.java
@@ -0,0 +1,13 @@
+package david.augusto.luan.msfolhadepagamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsFolhadepagamentoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsFolhadepagamentoApplication.class, args);
+    }
+
+}
---
+++ ms-folhadepagamento/src/test/java/david/augusto/luan/msfolhadepagamento/MsFolhadepagamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package david.augusto.luan.msfolhadepagamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsFolhadepagamentoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
